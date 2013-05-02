b751b1ad6559cd7bc56595d770b982c7855c3714
Mushroom <mushroom@res-51-13.dyn.ihouse-nyc.org>
2013-05-02 00:18:26 -0400

    Initial Commit

---
+++ Shapelet0502/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Shapelet0502
+//
+//  Created by Mushroom on 5/2/13.
+//  Copyright (c) 2013 Self. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
